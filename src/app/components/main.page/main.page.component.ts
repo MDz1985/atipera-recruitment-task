898838fc539160a17b1165809d90dba96df81eb1
MDz1985 <m_d@tut.by>
2024-09-16 21:46:38 +0200

    feat: use rxAngular instead of setTimeout or BehaviorSubject

--- src/app/components/main.page/main.page.component.ts
+++ src/app/components/main.page/main.page.component.ts
@@ -1,12 +1,19 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, Signal, signal } from '@angular/core';
 import { TableComponent } from 'src/app/components/main.page/components/table/table.component';
 import { DataStore } from 'src/app/store/data.store';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatDialog } from '@angular/material/dialog';
 import { EditElementModalComponent } from 'src/app/components/modals/edit-element.modal/edit-element.modal.component';
 import { PeriodicElementWithId } from 'src/app/models/interfaces/PeriodicElement';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Observable, startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { rxState } from '@rx-angular/state';
+import { RxLet } from '@rx-angular/template/let';
 
 const INPUT_DELAY_MS = 2000;
 
@@ -19,41 +26,56 @@ const INPUT_DELAY_MS = 2000;
     MatFormField,
     MatInput,
     MatLabel,
+    MatIcon,
+    MatIconButton,
+    MatSuffix,
+    FormsModule,
+    AsyncPipe,
+    RxLet,
   ],
   templateUrl: './main.page.component.html',
   styleUrl: './main.page.component.scss'
 })
 export class MainPageComponent {
-  private _filterTimeout: number | undefined;
   private readonly _store = inject(DataStore);
-  readonly filter = signal<string>('');
-  readonly data = this._store.data;
-  readonly isLoading = this._store.isLoading;
-  readonly dialog = inject(MatDialog);
+  private readonly _state = rxState<{ filter: string }>(({ set }) => {
+    set({ filter: '' });
+  });
+  private readonly _dialog: MatDialog = inject(MatDialog);
+  readonly filterInputValue = signal<string>('');
+  readonly data: Signal<PeriodicElementWithId[]> = this._store.data;
+  readonly isLoading: Signal<boolean> = this._store.isLoading;
+  readonly filter$: Observable<string> = this._state.select('filter').pipe(
+    distinctUntilChanged(),
+    debounceTime(INPUT_DELAY_MS),
+    startWith(''),
+  );
+
+  constructor() {
+    effect(() => {
+      this._state.set({ filter: this.filterInputValue() });
+    });
+  }
 
   openModal(data: [PeriodicElementWithId | undefined, MouseEvent]) {
     const [element, event] = data;
     if (element && event.target instanceof HTMLTableCellElement) {
-      const target = event.target as HTMLTableCellElement;
-      const columnIndex = target.cellIndex;
-      const columnName = Object.keys(element)[columnIndex];
-      const value = target.innerText;
-      this.dialog.open(EditElementModalComponent, { data: value }).afterClosed()
+      const target: HTMLTableCellElement = event.target;
+      const columnIndex: number = target.cellIndex;
+      const columnName: string = Object.keys(element)[columnIndex];
+      const cellValue: string = target.innerText;
+      this._dialog.open(EditElementModalComponent, { data: cellValue }).afterClosed()
         .subscribe((newValue: string) => {
           this.updateData(newValue !== undefined ? { ...element, [columnName]: newValue } : element);
         });
     }
   }
 
-  applyFilter(event: KeyboardEvent) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    clearTimeout(this._filterTimeout);
-    this._filterTimeout = window.setTimeout(() => {
-      this.filter.set(filterValue.trim().toLowerCase());
-    }, INPUT_DELAY_MS);
+  resetFilter(): void {
+    this.filterInputValue.set('');
   }
 
-  private updateData(data: PeriodicElementWithId) {
+  private updateData(data: PeriodicElementWithId): void {
     this._store.updateData(data);
   }
 }
